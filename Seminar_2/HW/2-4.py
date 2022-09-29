382e85c83810ff555a0749e6f93ab5730cc60ba1
Alena <ABelova0605@gmail.com>
2022-09-29 12:27:28 +0300

    add changes in HW task 2-4 in Seminar_2

--- Seminar_2/HW/2-4.py
+++ Seminar_2/HW/2-4.py
@@ -11,9 +11,33 @@ ls = list()
 for i in range(-abs(n), abs(n) + 1):
     ls.append(i)
 
-if pos_1 > len(ls) or pos_2 > len(ls):
-    print('Position is out of range.')
+if pos_1 > 0 and pos_2 > 0:
+    if pos_1 > len(ls) or pos_2 > len(ls):
+        print('Position is out of range.')
+    else:
+        result = ls[pos_1 - 1] * ls[pos_2 - 1]
+        print(ls)
+        print(result)
+elif pos_1 < 0 and pos_2 < 0:
+    if pos_1 < -len(ls) or pos_2 < -len(ls):
+        print('Position is out of range.')
+    else:
+        result = ls[pos_1] * ls[pos_2]
+        print(ls)
+        print(result)
+elif pos_1 > 0 and pos_2 < 0:
+    if pos_1 > len(ls) or pos_2 < -len(ls):
+        print('Position is out of range.')
+    else:
+        result = ls[pos_1 - 1] * ls[pos_2]
+        print(ls)
+        print(result)
+elif pos_1 < 0 and pos_2 > 0:
+    if pos_1 < -len(ls) or pos_2 > len(ls):
+        print('Position is out of range.')
+    else:
+        result = ls[pos_1] * ls[pos_2 - 1]
+        print(ls)
+        print(result)
 else:
-    result = ls[pos_1 - 1] * ls[pos_2 - 1]
-    print(ls)
-    print(result)
+    print('Error, 0 entered!')
