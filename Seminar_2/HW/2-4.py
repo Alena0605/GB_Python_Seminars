f28aa7d01b628c1f563d4f8ea175c98fba3bc96f
Alena <ABelova0605@gmail.com>
2022-10-03 14:53:18 +0300

    add changes in HW task 2-4 in Seminar_2

--- Seminar_2/HW/2-4.py
+++ Seminar_2/HW/2-4.py
@@ -6,38 +6,24 @@
 pos_1 = int(input('Enter the first position: '))
 pos_2 = int(input('Enter the second position: '))
 n = int(input('Enter a number: '))
-ls = list()
 
-for i in range(-abs(n), abs(n) + 1):
-    ls.append(i)
+ls = list(range(-abs(n), abs(n) + 1))
 
-if pos_1 > 0 and pos_2 > 0:
-    if pos_1 > len(ls) or pos_2 > len(ls):
-        print('Position is out of range.')
-    else:
-        result = ls[pos_1 - 1] * ls[pos_2 - 1]
-        print(ls)
-        print(result)
-elif pos_1 < 0 and pos_2 < 0:
-    if pos_1 < -len(ls) or pos_2 < -len(ls):
-        print('Position is out of range.')
-    else:
-        result = ls[pos_1] * ls[pos_2]
-        print(ls)
-        print(result)
-elif pos_1 > 0 and pos_2 < 0:
-    if pos_1 > len(ls) or pos_2 < -len(ls):
-        print('Position is out of range.')
-    else:
-        result = ls[pos_1 - 1] * ls[pos_2]
-        print(ls)
-        print(result)
-elif pos_1 < 0 and pos_2 > 0:
-    if pos_1 < -len(ls) or pos_2 > len(ls):
-        print('Position is out of range.')
-    else:
-        result = ls[pos_1] * ls[pos_2 - 1]
-        print(ls)
-        print(result)
+if len(ls) > pos_1 > 0 and len(ls)> pos_2 > 0:
+    result = ls[pos_1 - 1] * ls[pos_2 - 1]
+    print(ls)
+    print(result)
+elif -len(ls) < pos_1 < 0 and -len(ls) < pos_2 < 0:
+    result = ls[pos_1] * ls[pos_2]
+    print(ls)
+    print(result)
+elif len(ls) > pos_1 > 0 and -len(ls) < pos_2 < 0:
+    result = ls[pos_1 - 1] * ls[pos_2]
+    print(ls)
+    print(result)
+elif -len(ls) < pos_1 < 0 and len(ls) > pos_2 > 0:
+    result = ls[pos_1] * ls[pos_2 - 1]
+    print(ls)
+    print(result)
 else:
-    print('Error, 0 entered!')
+    print('There are no values for these indexes!')
